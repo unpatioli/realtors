77e79bec05b1d4c93140fdc3ff7daf63bc8ea1a6
msqui <msqui@yandex.ru>
2010-10-27 00:11:52 +0400

    static files processing ability added

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
+# -*- coding:utf-8 -*-
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,10 +11,19 @@ urlpatterns = patterns('',
     # (r'^realtors/', include('realtors.foo.urls')),
     (r'^$', 'welcome.views.index'),
     (r'^comments/', include('django.contrib.comments.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+# ¡¡¡ Only in dev mode !!!
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^{staticfiles_url}(?P<path>.*)$'.format(staticfiles_url=settings.STATICFILES_URL),
+            'django.views.static.serve',
+            {'document_root': settings.STATICFILES_DOC_ROOT}),
+    )
+
