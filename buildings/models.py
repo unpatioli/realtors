50fe4c02c969788f0095e53120d88c08278a4343
msqui <msqui@yandex.ru>
2010-10-26 17:29:41 +0400

    Rieltor model moved from buildings.model.Rieltor to accounts.model.Realtor with rename; model Building has field owner instead of rieltor. This field is ForeignKey to django.contrib.auth.model.User

--- buildings/models.py
+++ buildings/models.py
@@ -1,42 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Rieltor(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    experience = models.PositiveSmallIntegerField(null=True, blank=True)
-    is_private = models.BooleanField(default=False)
-    agency_title = models.CharField(max_length = 150, null=True, blank=True)
-    
-    in_sales = models.BooleanField(default=False)
-    in_rents = models.BooleanField(default=False)
-    in_camps = models.BooleanField(default=False)
-    in_commercials = models.BooleanField(default=False)
-    
-    in_msk = models.BooleanField(default=True)
-    in_country = models.BooleanField(default=False)
-    
-    commission_from = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    commission_to = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    
-    deal_commission = models.BooleanField(default=False)
-    
-    phone = models.CharField(max_length = 50, null=True, blank=True)
-    
-    description = models.TextField(null=True, blank=True)
-    
-    rating = models.PositiveSmallIntegerField(default=0)
-    views_count = models.PositiveIntegerField(default=0)
-    is_closed = models.BooleanField(default=0)
-    
-    avatar = models.ImageField(upload_to='avatars', null=True, blank=True)
-    
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
-    deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
-
-
 class Building(models.Model):
-    rieltor = models.ForeignKey(Rieltor)
+    owner = models.ForeignKey(User)
     town = models.CharField(max_length = 100)
     street = models.CharField(max_length = 100)
     house_id = models.CharField(max_length = 10)
