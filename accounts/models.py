fe8e9039157dd1848e570ac788f53c996e2b9c77
msqui <msqui@yandex.ru>
2010-10-26 17:22:48 +0400

    UserProfile model added to accounts.models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    GENDER_CHOICES = (
+        ('М', 'Муж'),
+        ('Ж', 'Жен'),
+    )
+    
+    user = models.ForeignKey(User, unique=True)
+    
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    
+    is_closed = models.BooleanField(default=False)
+    
+    description = models.TextField(null=True, blank=True)
+    
+    avatar = models.ImageField(upload_to='users/avatars', null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
+
