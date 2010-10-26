ec6b7dee290dcb13f5063c906085c981a90473d2
msqui <msqui@yandex.ru>
2010-10-26 20:25:07 +0400

    images.ImagedItem added as generic foreign key using django.contrib.contenttypes module

--- images/models.py
+++ images/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 
-# Create your models here.
+class ImagedItem(models.Model):
+    title = models.CharField(max_length=100, default="Без названия")
+    image = models.ImageField(upload_to='item_images')
+    
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    deleted_at = models.DateTimeField(null=True, blank=True, editable=False)
+    
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    
+    def __unicode__(self):
+        return self.title
+    
+
