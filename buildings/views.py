07ab2c5c1fa013cfcf6aca00a91ab570aad8a087
msqui <msqui@yandex.ru>
2010-10-27 13:26:38 +0400

    generic view object_list used to show flats

--- buildings/views.py
+++ buildings/views.py
@@ -1,13 +1,12 @@
 from buildings.models import RentFlat, SellFlat
 
 from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
 from django.views.generic.create_update import create_object, update_object
 
 def rent_flats_list(request):
-    rent_flats = RentFlat.objects.all()
-    return direct_to_template(
+    return object_list(
         request,
-        template='buildings/rent_flats_list.html',
-        extra_context={'rent_flats': rent_flats}
+        queryset=RentFlat.objects.all()
     )
 
