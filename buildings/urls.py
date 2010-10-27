dcc99de48f641edc11f39569467326d4d86d45f0
msqui <msqui@yandex.ru>
2010-10-27 13:47:03 +0400

    view names changed from [rent/sell]_flat_[..] to [rentflat/sellflat]_[...]; added rentflat_detail view and template; template_object_name parameter in generic views set to 'flat'

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,23 +1,25 @@
 from django.conf.urls.defaults import *
 
-rent_flats_patterns = patterns('buildings.views',
-    (r'^rent/flats$', 'rent_flats_list'),
-    (r'^rent/flats/new$', 'rent_flats_new'),
-    (r'^rent/flats/create$', 'rent_flats_create'),
-    (r'^rent/flats/edit/(?P<id>\d+)', 'rent_flats_edit'),
-    (r'^rent/flats/update/(?P<id>\d+)', 'rent_flats_update'),
-    (r'^rent/flats/delete/(?P<id>\d+)', 'rent_flats_delete')
+rentflat_patterns = patterns('buildings.views',
+    (r'^rent/flats$', 'rentflat_list'),
+    (r'^rent/flats/(?P<id>\d+)$', 'rentflat_detail'),
+    (r'^rent/flats/new$', 'rentflat_new'),
+    (r'^rent/flats/create$', 'rentflat_create'),
+    (r'^rent/flats/edit/(?P<id>\d+)', 'rentflat_edit'),
+    (r'^rent/flats/update/(?P<id>\d+)', 'rentflat_update'),
+    (r'^rent/flats/delete/(?P<id>\d+)', 'rentflat_delete')
 )
 
-sell_flats_patterns = patterns('buildings.views',
-    (r'^sell/flats$', 'sell_flats_list'),
-    (r'^sell/flats/new$', 'sell_flats_new'),
-    (r'^sell/flats/create$', 'sell_flats_create'),
-    (r'^sell/flats/edit/(?P<id>\d+)', 'sell_flats_edit'),
-    (r'^sell/flats/update/(?P<id>\d+)', 'sell_flats_update'),
-    (r'^sell/flats/delete/(?P<id>\d+)', 'sell_flats_delete')
+sellflat_patterns = patterns('buildings.views',
+    (r'^sell/flats$', 'sellflat_list'),
+    (r'^sell/flats/(?P<id>\d+)$', 'sellflat_detail'),
+    (r'^sell/flats/new$', 'sellflat_new'),
+    (r'^sell/flats/create$', 'sellflat_create'),
+    (r'^sell/flats/edit/(?P<id>\d+)', 'sellflat_edit'),
+    (r'^sell/flats/update/(?P<id>\d+)', 'sellflat_update'),
+    (r'^sell/flats/delete/(?P<id>\d+)', 'sellflat_delete')
 )
 
 urlpatterns = patterns('buildings.views', (r'^$', 'index')) + \
-    rent_flats_patterns + \
-    sell_flats_patterns
+    rentflat_patterns + \
+    sellflat_patterns
--- buildings/views.py
+++ buildings/views.py
@@ -1,12 +1,20 @@
 from buildings.models import RentFlat, SellFlat
 
 from django.views.generic.simple import direct_to_template
-from django.views.generic.list_detail import object_list
+from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object, update_object
 
-def rent_flats_list(request):
+def rentflat_list(request):
     return object_list(
         request,
-        queryset=RentFlat.objects.all()
+        queryset = RentFlat.objects.all(),
+        template_object_name = 'flat'
     )
 
+def rentflat_detail(request, id):
+    return object_detail(
+        request,
+        queryset = RentFlat.objects.all(),
+        object_id = id,
+        template_object_name = 'flat'
+    )
