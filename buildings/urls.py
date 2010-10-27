31d33410178165d97e33674f20d88437c75ef704
msqui <msqui@yandex.ru>
2010-10-27 12:00:47 +0400

    buildings urls changed from [rent/sell]_flats/[...] to [rent/sell]/flats/[...]

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,21 +1,21 @@
 from django.conf.urls.defaults import *
 
 rent_flats_patterns = patterns('buildings.views',
-    (r'^rent_flats$', 'rent_flats_list'),
-    (r'^rent_flats/new$', 'rent_flats_new'),
-    (r'^rent_flats/create$', 'rent_flats_create'),
-    (r'^rent_flats/edit/(?P<id>\d+)', 'rent_flats_edit'),
-    (r'^rent_flats/update/(?P<id>\d+)', 'rent_flats_update'),
-    (r'^rent_flats/delete/(?P<id>\d+)', 'rent_flats_delete')
+    (r'^rent/flats$', 'rent_flats_list'),
+    (r'^rent/flats/new$', 'rent_flats_new'),
+    (r'^rent/flats/create$', 'rent_flats_create'),
+    (r'^rent/flats/edit/(?P<id>\d+)', 'rent_flats_edit'),
+    (r'^rent/flats/update/(?P<id>\d+)', 'rent_flats_update'),
+    (r'^rent/flats/delete/(?P<id>\d+)', 'rent_flats_delete')
 )
 
 sell_flats_patterns = patterns('buildings.views',
-    (r'^sell_flats$', 'sell_flats_list'),
-    (r'^sell_flats/new$', 'sell_flats_new'),
-    (r'^sell_flats/create$', 'sell_flats_create'),
-    (r'^sell_flats/edit/(?P<id>\d+)', 'sell_flats_edit'),
-    (r'^sell_flats/update/(?P<id>\d+)', 'sell_flats_update'),
-    (r'^sell_flats/delete/(?P<id>\d+)', 'sell_flats_delete')
+    (r'^sell/flats$', 'sell_flats_list'),
+    (r'^sell/flats/new$', 'sell_flats_new'),
+    (r'^sell/flats/create$', 'sell_flats_create'),
+    (r'^sell/flats/edit/(?P<id>\d+)', 'sell_flats_edit'),
+    (r'^sell/flats/update/(?P<id>\d+)', 'sell_flats_update'),
+    (r'^sell/flats/delete/(?P<id>\d+)', 'sell_flats_delete')
 )
 
 urlpatterns = 
