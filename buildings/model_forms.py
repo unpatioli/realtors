ffe159a07e2b6f8019bc17f2cb3b6f55354816c5
msqui <msqui@yandex.ru>
2010-10-27 18:11:01 +0400

    buildings.model_forms forms divided by location to Moscow and Country forms. In view this division is represented by ?location=country GET parameter

--- buildings/model_forms.py
+++ buildings/model_forms.py
@@ -2,12 +2,27 @@ from buildings.models import RentFlat, SellFlat
 
 from django.forms import ModelForm
 
+# ==================
+# = RentFlat forms =
+# ==================
 class RentFlatForm(ModelForm):
     class Meta:
         model = RentFlat
-        exclude = ('owner',)
+        fields = ('town', 'street', 'house_id', 'building_id', 'total_area', 'price', 'currency', 'metro_remoteness_by_legs', 'metro_remoteness_by_bus', 'mkad_remoteness', 'nearest_metro_stations', 'description', 'house_type', 'renovation_type', 'is_new', 'furniture', 'fridge', 'wash_machine', 'separated_bathroom', 'parking', 'floor', 'floors_count', 'rooms_count', 'balcony_count', 'bathrooms_count', 'kitchen_area', 'payment_period', 'agent_commission', 'pets', 'children',)
+    
+class MoscowRentFlatForm(RentFlatForm):
+    class Meta(RentFlatForm.Meta):
+        exclude = ('town', 'mkad_remoteness',)
     
 
+class CountryRentFlatForm(RentFlatForm):
+    class Meta(RentFlatForm.Meta):
+        exclude = ('metro_remoteness_by_legs', 'metro_remoteness_by_bus', 'nearest_metro_stations', )
+
+
+# ==================
+# = SellFlat forms =
+# ==================
 class SellFlatForm(ModelForm):
     class Meta:
         model = SellFlat
--- buildings/views.py
+++ buildings/views.py
@@ -1,4 +1,4 @@
-from buildings.model_forms import RentFlatForm, SellFlatForm
+from buildings.model_forms import MoscowRentFlatForm, CountryRentFlatForm, SellFlatForm
 
 from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
@@ -20,15 +20,20 @@ def rentflat_detail(request, id):
     )
 
 def rentflat_new(request):
+    location = request.GET.get('location', '')
+    if location == 'country':
+        form_class = CountryRentFlatForm
+    else:
+        form_class = MoscowRentFlatForm
     return create_object(
         request,
-        form_class = RentFlatForm
+        form_class = form_class
     )
 
 def rentflat_edit(request, id):
     return update_object(
         request,
-        form_class = RentFlatForm,
+        form_class = MoscowRentFlatForm,
         object_id = id
     )
 
