4f32b7559f14fb447886528e124df59c32712dc1
msqui <msqui@yandex.ru>
2010-10-31 11:04:00 +0300

    location-based form dispatching added to views instead of direct form selection and generic views

--- buildings/views.py
+++ buildings/views.py
@@ -1,13 +1,19 @@
-from buildings.model_forms import MoscowRentFlatForm, CountryRentFlatForm, SellFlatForm
+# -*- coding:utf-8 -*-
+from buildings.models import RentFlat, SellFlat
+from buildings.form_dispatcher import FormDispatcher
 
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic.simple import direct_to_template, redirect_to
 from django.views.generic.list_detail import object_list, object_detail
-from django.views.generic.create_update import create_object, update_object
 
-def rentflat_list(request):
+
+def user_rentflat_list(request, user_id):
     return object_list(
         request,
-        queryset = RentFlat.objects.all(),
-        template_object_name = 'flat'
+        queryset = RentFlat.objects.filter(owner__pk__exact=user_id),
+        template_object_name = 'flat',
+        extra_context={'show_management_panel': request.user.id == user_id}
     )
 
 def rentflat_detail(request, id):
@@ -18,21 +24,53 @@ def rentflat_detail(request, id):
         template_object_name = 'flat'
     )
 
-def rentflat_new(request):
-    location = request.GET.get('location', '')
-    if location == 'country':
-        form_class = CountryRentFlatForm
+
+@login_required
+def rentflat_new(request, location):
+    form_dispatcher = FormDispatcher(deal_type='rent', location=location)
+    if not form_dispatcher.location_valid():
+        return redirect(rentflat_new, location='moscow')
+    
+    if request.method == 'POST':
+        instance = RentFlat(owner=request.user, **form_dispatcher.instance_params)
+        form = form_dispatcher.form_class(request.POST, instance=instance)
+        if form.is_valid():
+            flat = form.save()
+            
+            return redirect_to(request, url=flat.get_absolute_url(), permanent=False)
     else:
-        form_class = MoscowRentFlatForm
-    return create_object(
+        form = form_dispatcher.form_class()
+    return direct_to_template(
         request,
-        form_class = form_class
+        template = "buildings/rentflat_form.html",
+        extra_context = {'form': form}
     )
 
+@login_required
 def rentflat_edit(request, id):
-    return update_object(
+    # get object
+    flat = get_object_or_404(RentFlat, pk=id)
+    
+    # get form
+    form_dispatcher = FormDispatcher(deal_type='rent', location=flat.location)
+    # if not form_dispatcher.location_valid():
+    #     raise
+    
+    # process form
+    if request.method == 'POST':
+        form = form_dispatcher.form_class(request.POST, instance=flat)
+        if form.is_valid():
+            form.save()
+            return redirect_to(
+                request,
+                url=flat.get_absolute_url(),
+                permanent=False
+            )
+    else:
+        form = form_dispatcher.form_class(instance=flat)
+    return direct_to_template(
         request,
-        form_class = MoscowRentFlatForm,
-        object_id = id
+        template = "buildings/rentflat_form.html",
+        extra_context = {'form': form}
     )
 
