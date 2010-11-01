11717235a3338b75586c5c57523622a70f6bed30
msqui <msqui@yandex.ru>
2010-11-01 21:17:23 +0300

    location-based urls in buildings package are implemented as regexp, chosing from array buildings.location_dispatcher.LOCATION_FORMS

--- buildings/urls.py
+++ buildings/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls.defaults import *
 
+from buildings.location_dispatcher import LOCATION_FORMS
+
 common_patterns = patterns('buildings.views',
     # (r'/$', 'index'),
     # (r'user/(?P<user_id>\d+)/$', 'user_index'),
 )
 
-# TODO make <location> as array chose
+location_regexp = r"\b%s\b" % r"\b|\b".join([location for location in LOCATION_FORMS])
 rentflat_patterns = patterns('buildings.views',
     (r'user/(?P<user_id>\d+)/rent/flats/$', 'user_rentflat_list'),
     (r'rent/flats/(?P<id>\d+)/$', 'rentflat_detail'),
-    (r'rent/flats/new/(?P<location>\w+)/$', 'rentflat_new'),
+    (r'rent/flats/new/(?P<location>%s)/$' % location_regexp, 'rentflat_new'),
     (r'rent/flats/(?P<id>\d+)/edit/$', 'rentflat_edit'),
     # (r'rent/flats/(?P<id>\d+)/delete/$', 'rentflat_delete')
 )
