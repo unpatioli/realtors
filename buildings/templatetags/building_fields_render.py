df63f9cca6cbb698d93a130a15b7c1a3ada09285
msqui <msqui@yandex.ru>
2010-11-01 17:51:31 +0300

    in custom templatetag render_field it is now possible to push additional text between <li> tags using third parameter

--- buildings/templatetags/building_fields_render.py
+++ buildings/templatetags/building_fields_render.py
@@ -3,9 +3,12 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def render_field(name, value):
+def render_field(name, value, additional=None):
     res = ""
     if value:
-        res = "<li>%s: <span>%s</span></li>" % (name, value)
+        if additional is None:
+            res = "<li>%s: <span>%s</span></li>" % (name, value)
+        else:
+            res = "<li>%s: <span>%s</span> <span>%s</span></li>" % (name, value, additional)
     return res
 
