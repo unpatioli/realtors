b7ee64b691f1ebe47a81382279ea7a21326be13e
msqui <msqui@yandex.ru>
2010-11-05 22:05:22 +0300

    value escaping ability added to tags in buildings.templatetags.building_fields_render module. also 'flag_filed(value, text)' and 'pic_flag_field(value, src, title, alt)' simple tags added

--- buildings/templatetags/building_fields_render.py
+++ buildings/templatetags/building_fields_render.py
@@ -1,14 +1,32 @@
 from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.simple_tag
-def render_field(name, value, additional=None):
+def render_field(value, name, additional=None):
     res = ""
     if value:
+        esc = conditional_escape
         if additional is None:
-            res = "<li>%s: <span>%s</span></li>" % (name, value)
+            res = "<li>%s: <span>%s</span></li>" % (name, esc(value))
         else:
-            res = "<li>%s: <span>%s</span> <span>%s</span></li>" % (name, value, additional)
+            res = "<li>%s: <span>%s</span> <span>%s</span></li>" % (name, esc(value), additional)
+    return mark_safe(res)
+render_field.needs_autoescape = True
+
+@register.simple_tag
+def flag_field(value, text):
+    res = ""
+    if value:
+        res = "<li>%s</li>" % text
+    return res
+
+@register.simple_tag
+def pic_flag_field(value, src, title="", alt=""):
+    res = ""
+    if value:
+        res = '<img src="%s" title="%s" alt="%s" />' % (src, title, alt)
     return res
 
