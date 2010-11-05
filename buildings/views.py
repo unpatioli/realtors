f91b490732b9700f3156c8fb2b42b648f3d72fed
msqui <msqui@yandex.ru>
2010-11-05 22:08:04 +0300

    more generic template buildings/flat_form.html is used instead of buildings/rentflat_form.html; 'flat.can_edit(user)' check is applied; SellFlat views added

--- buildings/views.py
+++ buildings/views.py
@@ -2,17 +2,20 @@
 from buildings.models import RentFlat, SellFlat
 from buildings.location_dispatcher import LocationDispatcher
 
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template, redirect_to
 from django.views.generic.list_detail import object_list
 
 
+# ============
+# = RentFlat =
+# ============
 def user_rentflat_list(request, user_id):
     return object_list(
         request,
         queryset = RentFlat.objects.filter(owner__pk__exact=user_id),
-        template_object_name = 'flat',
         extra_context = {
             'objects_type': 'rent_flats',
             'show_management_panel': request.user.id == int(user_id),
@@ -28,7 +31,10 @@ def rentflat_detail(request, id):
     return direct_to_template(
         request,
         template = dispatcher.detail_template,
-        extra_context = {'flat': flat}
+        extra_context = {
+            'object': flat,
+            'show_object_controls': flat.can_edit(request.user),
+        }
     )
 
 
@@ -49,14 +55,22 @@ def rentflat_new(request, location):
         form = form_dispatcher.form_class()
     return direct_to_template(
         request,
-        template = "buildings/rentflat_form.html",
-        extra_context = {'form': form}
+        template = "buildings/flat_form.html",
+        extra_context = {
+            'form': form,
+            'locations': form_dispatcher.localized_titles('ru'),
+            'location': location,
+            'deal_type': 'rent',
+        }
     )
 
 @login_required
 def rentflat_edit(request, id):
     # get object
     flat = get_object_or_404(RentFlat, pk=id)
+    
+    if not flat.can_edit(request.user):
+        raise Http404
     
     # get form
     form_dispatcher = LocationDispatcher(deal_type='rent', location=flat.location)
@@ -78,7 +92,93 @@ def rentflat_edit(request, id):
         form = form_dispatcher.form_class(instance=flat)
     return direct_to_template(
         request,
-        template = "buildings/rentflat_form.html",
+        template = "buildings/flat_form.html",
+        extra_context = {'form': form}
+    )
+
+
+# =============
+# = SellFlats =
+# =============
+def user_sellflat_list(request, user_id):
+    return object_list(
+        request,
+        queryset = SellFlat.objects.filter(owner__pk__exact=user_id),
+        extra_context = {
+            'objects_type': 'sell_flats',
+            'show_management_panel': request.user.id == int(user_id),
+            'user_id': user_id,
+        },
+    )
+
+def sellflat_detail(request, id):
+    flat = get_object_or_404(SellFlat, pk=id)
+    dispatcher = LocationDispatcher(deal_type='sell', location=flat.location)
+    # TODO check if dispatcher has wrong location
+    
+    return direct_to_template(
+        request,
+        template = dispatcher.detail_template,
+        extra_context = {
+            'object': flat,
+            'show_object_controls': flat.can_edit(request.user),
+        }
+    )
+
+
+@login_required
+def sellflat_new(request, location):
+    form_dispatcher = LocationDispatcher(deal_type='sell', location=location)
+    if not form_dispatcher.location_valid():
+        return redirect(sellflat_new, location='moscow')
+    
+    if request.method == 'POST':
+        instance = SellFlat(owner=request.user, **form_dispatcher.instance_params)
+        form = form_dispatcher.form_class(request.POST, instance=instance)
+        if form.is_valid():
+            flat = form.save()
+            
+            return redirect_to(request, url=flat.get_absolute_url(), permanent=False)
+    else:
+        form = form_dispatcher.form_class()
+    return direct_to_template(
+        request,
+        template = "buildings/flat_form.html",
+        extra_context = {
+            'form': form,
+            'locations': form_dispatcher.localized_titles('ru'),
+            'location': location,
+            'deal_type': 'sell',
+        }
+    )
+
+@login_required
+def sellflat_edit(request, id):
+    # get object
+    flat = get_object_or_404(SellFlat, pk=id)
+    
+    if not flat.can_edit(request.user):
+        raise Http404
+    
+    # get form
+    form_dispatcher = LocationDispatcher(deal_type='sell', location=flat.location)
+    # TODO check if form_dispatcher has wrong location
+    
+    # process form
+    if request.method == 'POST':
+        form = form_dispatcher.form_class(request.POST, instance=flat)
+        if form.is_valid():
+            form.save()
+            return redirect_to(
+                request,
+                url=flat.get_absolute_url(),
+                permanent=False
+            )
+    else:
+        form = form_dispatcher.form_class(instance=flat)
+    return direct_to_template(
+        request,
+        template = "buildings/flat_form.html",
         extra_context = {'form': form}
     )
 
