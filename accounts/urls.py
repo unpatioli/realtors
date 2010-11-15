d44ee72790174a9162438454cfc8ef2e87a46e87
msqui <msqui@yandex.ru>
2010-11-15 15:59:47 +0300

    accounts package urls now use 'url' function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,44 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
-    (r'logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logged_out.html'}),
-    (r'register/$', 'accounts.views.register'),
+    url(    r'login/$', 
+            'django.contrib.auth.views.login',
+            {'template_name': 'accounts/login.html'},
+            name = 'accounts_login'
+    ),
+    url(    r'logout/$',
+            'django.contrib.auth.views.logout',
+            {'template_name': 'accounts/logged_out.html'},
+            name = 'accounts_logout'
+    ),
+    url(    r'register/$',
+            'accounts.views.register',
+            name = 'accounts_register'
+    ),
     
-    (r'profile/(?P<user_id>\d+)/$', 'accounts.views.profile'),
-    (r'profile/$', 'accounts.views.my_profile'),
-    (r'profile/new/$', 'accounts.views.my_profile_new'),
-    (r'profile/edit/$', 'accounts.views.my_profile_edit'),
+    url(    r'profile/(?P<user_id>\d+)/$',
+            'accounts.views.profile',
+            name = 'accounts_profile'
+    ),
+    url(    r'profile/$',
+            'accounts.views.my_profile',
+            name = 'accounts_my_profile'
+    ),
+    url(    r'profile/new/$',
+            'accounts.views.my_profile_new',
+            name = 'accounts_my_profile_new'
+    ),
+    url(    r'profile/edit/$',
+            'accounts.views.my_profile_edit',
+            name = 'accounts_my_profile_edit'),
     
-    (r'profile/realtor/new/$', 'accounts.views.realtor_new'),
-    (r'profile/realtor/edit/$', 'accounts.views.realtor_edit'),
+    url(    r'profile/realtor/new/$',
+            'accounts.views.realtor_new',
+            name = 'accounts_my_profile_realtor_new'
+    ),
+    url(    r'profile/realtor/edit/$',
+            'accounts.views.realtor_edit',
+            name = 'accounts_my_profile_realtor_edit'
+    ),
     
 )
