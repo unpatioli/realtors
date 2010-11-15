dc441cce26eb377b6604037dc7c964b161bb2778
msqui <msqui@yandex.ru>
2010-11-15 17:05:37 +0300

    redirect_to changed to redirect in accounts package

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,8 @@ from accounts.forms import RegistrationForm, UserprofileForm, RealtorForm
 from accounts.models import UserProfile, Realtor
 
 from django.contrib.auth.decorators import login_required
-from django.views.generic.simple import direct_to_template, redirect_to
+from django.views.generic.simple import direct_to_template
+from django.shortcuts import redirect
 
 def register(request):
     from django.contrib.auth.models import User
@@ -19,7 +20,7 @@ def register(request):
             user.last_name = form.cleaned_data["last_name"]
             user.save()
             
-            return redirect_to(request, url='/', permanent=False)
+            return redirect('/')
     else:
         form = RegistrationForm()
     
@@ -31,7 +32,7 @@ def register(request):
 
 def profile(request, user_id):
     if request.user.is_authenticated() and request.user.id == int(user_id):
-        return redirect_to(request, url='/accounts/profile/', permanent=False)
+        return redirect(my_profile)
     try:
         userprofile = UserProfile.objects.get(user=user_id)
     except UserProfile.DoesNotExist:
@@ -50,7 +51,7 @@ def my_profile(request):
     try:
         userprofile = request.user.get_profile()
     except UserProfile.DoesNotExist:
-        return redirect_to(request, url="/accounts/profile/new/", permanent=False)
+        return redirect(my_profile_new)
     
     return direct_to_template(
         request,
@@ -61,7 +62,7 @@ def my_profile(request):
 @login_required
 def my_profile_new(request):
     if request.user.userprofile_set.exists():
-        return redirect_to(request, url="/accounts/profile/edit/", permanent=False)
+        return redirect(my_profile_edit)
     if request.method == 'POST':
         form = UserprofileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -69,7 +70,7 @@ def my_profile_new(request):
             up.user = request.user
             up.save()
             
-            return redirect_to(request, url=up.get_absolute_url(), permanent=False)
+            return redirect(up)
     else:
         form = UserprofileForm()
     
@@ -84,17 +85,13 @@ def my_profile_edit(request):
     try:
         userprofile = request.user.get_profile()
     except UserProfile.DoesNotExist:
-        return redirect_to(request, url="/accounts/profile/new/", permanent=False)
+        return redirect(my_profile_new)
     
     if request.method == 'POST':
         form = UserprofileForm(request.POST, request.FILES, instance=userprofile)
         if form.is_valid():
             form.save()
-            return redirect_to(
-                request,
-                url=userprofile.get_absolute_url(),
-                permanent=False
-            )
+            return redirect(userprofile)
     else:
         form = UserprofileForm(instance=userprofile)
     
@@ -107,7 +104,7 @@ def my_profile_edit(request):
 @login_required
 def realtor_new(request):
     if request.user.realtor_set.exists():
-        return redirect_to(request, url="/accounts/profile/realtor/edit", permanent=False)
+        return redirect(realtor_edit)
     if request.method == 'POST':
         form = RealtorForm(request.POST)
         if form.is_valid():
@@ -115,7 +112,7 @@ def realtor_new(request):
             r.user = request.user
             r.save()
             
-            return redirect_to(request, url=r.get_absolute_url(), permanent=False)
+            return redirect(r)
     else:
         form = RealtorForm()
     
@@ -130,17 +127,13 @@ def realtor_edit(request):
     try:
         realtor = request.user.realtor_set.get()
     except Realtor.DoesNotExist:
-        return redirect_to(request, url="/accounts/profile/realtor/new", permanent=False)
+        return redirect(realtor_new)
     
     if request.method == 'POST':
         form = RealtorForm(request.POST, instance=realtor)
         if form.is_valid():
             form.save()
-            return redirect_to(
-                request,
-                url=realtor.get_absolute_url(),
-                permanent=False
-            )
+            return redirect(realtor)
     else:
         form = RealtorForm(instance=realtor)
     
