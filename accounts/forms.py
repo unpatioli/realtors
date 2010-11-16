9ca20ca40dcb79ec692f1bbc79a1acd7c6b6c55d
msqui <msqui@yandex.ru>
2010-11-16 17:13:44 +0300

    User model inclusion fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from django import forms
+from django.contrib.auth.models import User
 from accounts.models import UserProfile, Realtor
 
 class RegistrationForm(forms.Form):
