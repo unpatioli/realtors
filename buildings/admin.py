49b24ca554a4668ced8ad9bf7e3d1cf2a0812930
msqui <msqui@yandex.ru>
2010-11-24 13:31:41 +0300

    buildings.Metro model added to admin

--- buildings/admin.py
+++ buildings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from buildings import models
 
+admin.site.register(models.Metro)
+
 admin.site.register(models.ExtraParameters)
 admin.site.register(models.RentFlat)
 admin.site.register(models.SellFlat)
